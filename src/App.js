9d70df720a26bf1bdc11671d120cb8676e228750
Abul Kashem <kasemshishir1100@gmail.com>
2022-08-30 17:03:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import chillHop from "./utils/data";
 
-function App() {
+const App = () => {
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
+  console.log(currentSong);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <h1>Music App</h1>
+    </main>
   );
-}
+};
 
 export default App;
